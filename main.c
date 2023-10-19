f1e7a618b555da0fc8815909869c3bc9fc0d7c81
minjiwkim <minji5489235790@gmail.com>
2023-10-19 15:47:32 +0900

    4

--- main.c
+++ main.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int inc(int counter);
+int counter;
+
+void set_counter()
+{
+     counter = 20;
+}
 int main(int argc, char *argv[])
 {
-   int i = 10;
-   printf("함수 호출전 i=%d\n", i);
-   inc(i);
-   printf("함수 호출후 i=%d\n", i);
-  
+    printf("counter=%d\n", counter);
+    set_counter();
+    printf("counter=%d\n", counter);
+
   system("PAUSE");	
   return 0;
-}
-
-int inc(int counter)
-{
-    counter++;
-    return counter;
 }
